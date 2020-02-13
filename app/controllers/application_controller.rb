884a5c68c5c940f872012d5b441c05676bbf93ad
yj <t61.yamamoto.jinya@gmail.com>
2020-02-13 16:44:01 +0900

    mypage_create

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,7 +4,8 @@ class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
 
   def after_sign_in_path_for(resource)
-    "/user/#{current_user.id}"
+    user_path(resource.id)
+    #"/user/#{current_user.id}"deviseのデフォルトパス
   end
   
   protected
