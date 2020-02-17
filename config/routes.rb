a9f890a0a4a19027a12eb530ca30239bbc0632cb
yj <t61.yamamoto.jinya@gmail.com>
2020-02-17 16:18:25 +0900

    post_page_create

--- config/routes.rb
+++ config/routes.rb
@@ -4,4 +4,6 @@ Rails.application.routes.draw do
   devise_for :users
   resources :users, :only => [:show]
   get 'plans' => 'plans#index'
+  get 'plans/new' => 'plans#new'
+  post 'plans/new' => 'plans#new'
 end
