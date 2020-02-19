172c61fde3de7801b37356b205257235bb8c0fcf
yj <t61.yamamoto.jinya@gmail.com>
2020-02-19 23:38:41 +0900

    post_function

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20200128162957) do
+ActiveRecord::Schema.define(version: 20200217072825) do
+
+  create_table "plans", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
+    t.string   "title"
+    t.text     "details",    limit: 65535
+    t.string   "image"
+    t.string   "region"
+    t.date     "date"
+    t.datetime "created_at",               null: false
+    t.datetime "updated_at",               null: false
+  end
 
   create_table "users", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.string   "email",                  default: "", null: false
